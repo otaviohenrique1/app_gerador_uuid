706962997b750e92c1025ae26008a4b32e188720
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-09 13:13:58 -0300

    commit 4

--- bin/app_gerador_uuid.dart
+++ bin/app_gerador_uuid.dart
@@ -2,4 +2,7 @@ import 'package:app_gerador_uuid/app_gerador_uuid.dart' as app_gerador_uuid;
 
 void main(List<String> arguments) {
   app_gerador_uuid.geradorUuid();
+  // app_gerador_uuid.geradorDataHoraFormatada();
+  // app_gerador_uuid.geradorHoraFormatada();
+  // app_gerador_uuid.geradorDataFormatada();
 }
--- lib/app_gerador_uuid.dart
+++ lib/app_gerador_uuid.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 import 'dart:io';
 
@@ -13,11 +14,74 @@ void geradorUuid() {
   print("----------------------------------------");
 }
 
+void geradorDataHoraFormatada() {
+  print("----------------------------------------");
+  print("Gerador de data e hora formatada");
+  stdout.write("Digite a quantidade: ");
+  int quantidade = int.parse(stdin.readLineSync()!);
+  List<String> lista = gerarListaDataHoraFormatada(quantidade);
+  for (var item in lista) {
+    print(item);
+  }
+  print("----------------------------------------");
+}
+
+void geradorHoraFormatada() {
+  print("----------------------------------------");
+  print("Gerador de hora formatada");
+  stdout.write("Digite a quantidade: ");
+  int quantidade = int.parse(stdin.readLineSync()!);
+  List<String> lista = geraListaHoraFormatada(quantidade);
+  for (var item in lista) {
+    print(item);
+  }
+  print("----------------------------------------");
+}
+
+void geradorDataFormatada() {
+  print("----------------------------------------");
+  print("Gerador de data formatada");
+  stdout.write("Digite a quantidade: ");
+  int quantidade = int.parse(stdin.readLineSync()!);
+  List<String> lista = geraListaDataFormatada(quantidade);
+  for (var item in lista) {
+    print(item);
+  }
+  print("----------------------------------------");
+}
+
 List<String> gerarListaUuid(int quantidade) {
   List<String> lista = [];
   var uuid = Uuid();
   for (var i = 0; i < quantidade; i++) {
     lista.add("${i + 1} => ${uuid.v4()}");
+  }
+  return lista;
+}
+
+List<String> gerarListaDataHoraFormatada(int quantidade) {
+  List<String> lista = [];
+  String dataHora = DateFormat('dd/MM/yyyy HH:mm:ss').format(DateTime.now());
+  for (var i = 0; i < quantidade; i++) {
+    lista.add("${i + 1} => $dataHora");
+  }
+  return lista;
+}
+
+List<String> geraListaHoraFormatada(int quantidade) {
+  List<String> lista = [];
+  String hora = DateFormat('HH:mm:ss').format(DateTime.now());
+  for (var i = 0; i < quantidade; i++) {
+    lista.add("${i + 1} => $hora");
+  }
+  return lista;
+}
+
+List<String> geraListaDataFormatada(int quantidade) {
+  List<String> lista = [];
+  String hora = DateFormat('dd/MM/yyyy').format(DateTime.now());
+  for (var i = 0; i < quantidade; i++) {
+    lista.add("${i + 1} => $hora");
   }
   return lista;
 }
